00a492c735ac8deba9f3c2956120314d08aa47a8
liukaixin01 <1360517661@qq.com>
2017-08-12 15:45:17 +0800

    [add] react-hot-loader

--- app/index.js
+++ app/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import {render} from 'react-dom'
+import {AppContainer} from 'react-hot-loader'
 import Hello from './components/hello'
 
 render (
-  <Hello />,
+  <AppContainer>
+    <Hello />
+  </AppContainer>,
   document.getElementById('root')
 )
+
+if(module.hot) {
+  module.hot.accept('./components/hello', () => {
+    const NewHello = require('./components/hello').default;
+    render(
+      <AppContainer>
+        <NewHello />
+      </AppContainer>,
+      document.getElementById('root')
+    )
+  })
+}
