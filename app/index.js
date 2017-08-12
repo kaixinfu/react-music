4a13b4ed1e06b0de91fbb2200455dd865cd656fb
liukaixin01 <1360517661@qq.com>
2017-08-12 15:00:47 +0800

    [add] webpack loader

--- app/index.js
+++ app/index.js
@@ -1,2 +1,3 @@
-var react = require('react');
+import react from 'react';
+import './index.less'
 console.log(react.version)
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,26 @@ module.exports = {
   output: {
     path: './dist',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query:
+          {
+            presets:['react','es2015']
+          }
+      },
+      {
+          test: /\.css$/,
+          loader: "style!css"
+      },
+      {
+          test: /\.less/,
+          loader: 'style-loader!css-loader!less-loader'
+      }
+    ]
   }
 }
